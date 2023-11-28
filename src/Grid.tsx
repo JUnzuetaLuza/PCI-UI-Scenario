ce0abc8bf86b68d37828de261722c825589f9512
joseu <joseu@DESKTOP-4GF1JQ4>
2023-11-28 14:07:34 -0500

    Task #3

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -11,7 +11,16 @@ const defaultColDef = {
 
 const columnDefs: ColDef[] = [
   { field: "designation", filter: 'agTextColumnFilter', headerName: "Designation" },
-  { field: "discovery_date", filter: 'agDateColumnFilter', headerName: "Discovery Date", },
+  { field: "discovery_date", filter: 'agDateColumnFilter', headerName: "Discovery Date",
+    valueFormatter: (data) => {
+      const date = new Date(data.value);
+      const formattedDate = date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+      });
+      return formattedDate;
+  }},
   { field: "h_mag", headerName: "H (mag)" },
   { field: "moid_au", headerName: "MOID (au)" },
   { field: "q_au_1", headerName: "q (au)" },
