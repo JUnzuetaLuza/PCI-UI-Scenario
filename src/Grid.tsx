ff0652ccef8b595117726d1b2eea84fb9a8af6f6
joseu <joseu@DESKTOP-4GF1JQ4>
2023-11-28 15:11:31 -0500

    Task #6

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -4,6 +4,7 @@ import data from "./near-earth-asteroids.json";
 import "ag-grid-enterprise";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { useRef } from "react";
 import "./style.css";
 
 const defaultColDef = { 
@@ -41,11 +42,18 @@ const columnDefs: ColDef[] = [
 ];
 
 const NeoGrid = (): JSX.Element => {
+  const refGrid = useRef<AgGridReact>(null);
+  const clearFiltSort = () => {
+    refGrid.current?.api.setFilterModel(null);
+    refGrid.current?.columnApi?.applyColumnState({
+      defaultState: { sort: null },
+    })
+  }
   return (
     <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
       <div className="topGrid">
         <h1 className="title">Near-Earth Object Overview</h1>
-        <button className="clearFiltSort">Clear Filters and Sorters</button>
+        <button className="clearFiltSort" onClick={clearFiltSort}>Clear Filters and Sorters</button>
       </div>
       <AgGridReact
         rowData={data}
@@ -53,6 +61,7 @@ const NeoGrid = (): JSX.Element => {
         columnDefs={columnDefs}
         rowGroupPanelShow={'always'}
         enableRangeSelection={true}
+        ref={refGrid}
       />
     </div>
   );
